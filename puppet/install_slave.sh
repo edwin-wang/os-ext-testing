7d864eaec64fe29837b389e2803dec7bc37e83bd
Edwin Wang <edwin.wang@cn.ibm.com>
2015-07-28 10:05:49 -0500

    Modify os-ext-testing path in install_slave.sh

--- puppet/install_slave.sh
+++ puppet/install_slave.sh
@@ -9,7 +9,7 @@ THIS_DIR=`pwd`
 DATA_REPO_INFO_FILE=$THIS_DIR/.data_repo_info
 DATA_PATH=$THIS_DIR/os-ext-testing-data
 OSEXT_PATH=$THIS_DIR/os-ext-testing
-OSEXT_REPO=https://github.com/rasselin/os-ext-testing
+OSEXT_REPO=https://github.com/edwin-wang/os-ext-testing
 PUPPET_MODULE_PATH="--modulepath=$OSEXT_PATH/puppet/modules:/root/system-config/modules:/etc/puppet/modules"
 
 if ! sudo test -d /root/system-config; then
